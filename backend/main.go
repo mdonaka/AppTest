c05675a17b188beac374c28631ec4e867a4cc179
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2025-03-26 17:01:42 +0900

    clean go fmt

--- backend/main.go
+++ backend/main.go
@@ -15,9 +15,9 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		response := Response{Message: "Hello, World!"}
 		w.Header().Set("Content-Type", "application/json")
-    w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-    w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
-    w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		w.WriteHeader(http.StatusOK)
 		if err := json.NewEncoder(w).Encode(response); err != nil {
