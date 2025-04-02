05b550e47e0d05793d8b6e6de3b401d4d9214e53
mdonaka <4ryo2-meron-math@ezweb.ne.jp>
2025-04-03 01:42:43 +0900

    [database] update database

--- database/import_spices.sh
+++ database/import_spices.sh
@@ -3,6 +3,7 @@
 DB_NAME="/data/spices.db"
 CSV_FILE="spices.csv"
 TABLE_NAME="spices"
+TEMP_TABLE="tmp"
 
 if [[ ! -f "$CSV_FILE" ]]; then
     echo "$CSV_FILE is not found."
@@ -10,16 +11,36 @@ if [[ ! -f "$CSV_FILE" ]]; then
 fi
 
 sqlite3 "$DB_NAME" <<EOF
+
+# 一時的にCSVからテーブルを作成
+DROP TABLE IF EXISTS $TEMP_TABLE;
+CREATE TABLE $TEMP_TABLE (
+    name TEXT,
+    alias TEXT,
+    taste TEXT,
+    flavor TEXT,
+    family TEXT,
+    origin TEXT
+);
+.mode csv
+.import $CSV_FILE $TEMP_TABLE
+
+
+# IDを追加して最終的なテーブルを作成
 DROP TABLE IF EXISTS $TABLE_NAME;
 CREATE TABLE $TABLE_NAME (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
+    alias TEXT,
+    taste TEXT,
     flavor TEXT,
-    family TEXT
+    family TEXT,
+    origin TEXT
 );
 
-.mode csv
-.import $CSV_FILE $TABLE_NAME
+INSERT INTO $TABLE_NAME (name, alias, taste, flavor, family, origin)
+SELECT name, alias, taste, flavor, family, origin FROM $TEMP_TABLE;
+DROP TABLE $TEMP_TABLE;
 
 SELECT * FROM $TABLE_NAME;
 EOF
